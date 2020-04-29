8e26c078227ec3519d216c1326c2f352cf8efa1d
Bryson Gullett <brysongullett@gmail.com>
2020-04-29 14:12:25 -0400

    Added test product delete capability to api

--- models/Product.php
+++ models/Product.php
@@ -120,7 +120,7 @@ class Product {
         $stmt->bindParam(":created", $this->created);
         $stmt->bindParam(":modified", $this->modified);
 
-        // Execure query
+        // Execute query
         if($stmt->execute()) {
             return true;
         }
@@ -167,7 +167,35 @@ class Product {
         $stmt->bindParam(":modified", $this->modified);
         $stmt->bindParam(":id", $this->id);
 
-        // Execure query
+        // Execute query
+        if($stmt->execute()) {
+            return true;
+        }
+
+        // Print error if something goes wrong
+        printf("Error: %s.\n", $stmt->error);
+
+        return false;
+    }
+
+    // Delete product
+    public function delete() {
+        // Create query
+        $query = "DELETE FROM ". $this->table ."
+            WHERE
+                id = :id
+        ";
+
+        // Prepared statement
+        $stmt = $this->conn->prepare($query);
+
+        // Clean data
+        $this->id = htmlspecialchars(strip_tags($this->id));
+
+        // Bind data
+        $stmt->bindParam(":id", $this->id);
+
+        // Execute query
         if($stmt->execute()) {
             return true;
         }
